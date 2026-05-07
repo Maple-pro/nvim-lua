88a1be43220f9e79b80f3860c6729f725287d2db
Maples <1719957182@qq.com>
2026-05-07 22:02:50 +0800

    feat: update surround plugin and lsp config

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -4,23 +4,23 @@ local mason_lspconfig = require("mason-lspconfig")
 mason.setup()
 
 local servers = {
-    bashls,
-    clangd,
-    cssls,
-    emmet_ls,
-    gopls,
-    html,
-    jsonls,
-    lua_ls,
-    pyright,
-    rust_analyzer,
-    ts_ls,
-    yamlls,
+    "bashls",
+    "clangd",
+    "cssls",
+    "emmet_ls",
+    "gopls",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "pyright",
+    "rust_analyzer",
+    "ts_ls",
+    "yamlls",
 }
 
-mason_lspconfig.setup({
-  ensure_installed = vim.tbl_keys(servers),
-})
+mason_lspconfig.setup{
+  ensure_installed = servers,
+}
 
 -- 全局 LSP 附加功能：快捷键、自动命令、功能配置
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -38,8 +38,4 @@ vim.api.nvim_create_autocmd("LspAttach", {
     require("keybindings").mapLSP(buf_set_keymap)
   end,
 })
-
-for _, server in ipairs(servers) do
-    vim.lsp.enable(server)
-end
 
--- lua/plugin-config/leap.lua
+++ lua/plugin-config/leap.lua
@@ -4,5 +4,5 @@ if not status then
   return
 end
 
-vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
-vim.keymap.set('n',               'S', '<Plug>(leap-from-window)')
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>s', '<Plug>(leap)')
+vim.keymap.set('n',               '<leader>S', '<Plug>(leap-from-window)')
